18e746849e283e9b62ea161a6bff599f7eb05480
Andres Mejias <aemabit@gmail.com>
2020-01-13 01:15:54 -0500

    details

--- index.js
+++ index.js
@@ -41,15 +41,66 @@ const instagram = async () => {
   //   console.log(TAKEUSER);                                         //
   //////////////////////////////////////////////////////////////////////
 
+  // FIRST STEP TAKE PERSONAL USER INFO FROM INSTAGRAM
+
+  //   let instagram_user = {
+  //     followers: user.edge_followed_by.count,
+  //     following: user.edge_follow.count,
+  //     uploads: user.edge_owner_to_timeline_media.count,
+  //     full_name: user.full_name,
+  //     picture: user.profile_pic_url_hd
+  //   };
+
+  //////////////////////////////////////
+  //   HERE TAKE MY BASIC INFO USER  //
+  //   console.log(instagram_user);   //
+  //////////////////////////////////////
+
+  // TAKE POSTS FROM USER
+  let {
+    entry_data: {
+      ProfilePage: {
+        [0]: {
+          graphql: {
+            user: {
+              edge_owner_to_timeline_media: { edges }
+            }
+          }
+        }
+      }
+    }
+  } = JSON.parse(script_regex[1]);
+
+  let posts = [];
+
+  // GO TO INSIDE OF EACH POST
+  for (let edge of edges) {
+    //take the node inside of each POST
+    let { node } = edge;
+
+    // PUSH TO AN ARRAY TO TAKE THE DATA
+    posts.push({
+      id: node.id,
+      shortcode: node.shortcode,
+      timestamp: node.taken_at_timestamp,
+      likes: node.edge_liked_by.count,
+      comments: node.edge_media_to_comment.count,
+      video_views: node.video_view_count,
+      caption: node.edge_media_to_caption.edges[0].node.text,
+      image_url: node.display_url
+    });
+  }
+
+  // ADD THE POSTS ARRAY TO YOUR JSON DATA USER
+
   let instagram_user = {
     followers: user.edge_followed_by.count,
     following: user.edge_follow.count,
     uploads: user.edge_owner_to_timeline_media.count,
     full_name: user.full_name,
-    picture: user.profile_pic_url_hd
+    picture: user.profile_pic_url_hd,
+    posts
   };
-
-  console.log(instagram_user);
 
   // USE DEBUGGER
   debugger;
